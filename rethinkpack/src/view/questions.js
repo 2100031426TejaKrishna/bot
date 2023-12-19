3b33ca8cc3b11b1b195fb04f02689b073a030514
Chen Shiang <chenshianglee@gmail.com>
2023-12-19 18:00:31 +0800

    display question

--- rethinkpack/src/view/questions.js
+++ rethinkpack/src/view/questions.js
@@ -4,6 +4,23 @@ import EditQuestion from './editQuestion';
 
 const Questions = () => {
     const [questions, setQuestions] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedQuestion, setSelectedQuestion] = useState(null);
+
+    const handleDeleteClick = (questionId) => {
+        setSelectedQuestion(questionId);
+        setIsModalOpen(true);
+    };
+
+    const handleConfirmDelete = async (questionId) => {
+        setIsModalOpen(false);
+        try {
+            await fetch(`http://localhost:5000/api/deleteQuestion/${questionId}`, { method: 'DELETE' });
+            setQuestions(questions.filter(question => question._id !== questionId));
+        } catch (error) {
+            console.error("Error deleting question:", error);
+        }
+    };
 
     const formatDate = (dateString) => {
         const options = { year: 'numeric', month: '2-digit', day: '2-digit'};
@@ -26,6 +43,21 @@ const Questions = () => {
 
         fetchQuestions();
     }, []);
+
+    const ConfirmModal = ({ isOpen, onClose, onConfirm, question }) => {
+        if (!isOpen) return null;
+      
+        return (
+          <div className="modal-backdrop">
+            <div className="modal">
+              <h4>Delete Question</h4>
+              <p>Are you sure you want to delete this question?</p>
+              <button onClick={onClose}>Cancel</button>
+              <button onClick={() => onConfirm(question)}>Confirm</button>
+            </div>
+          </div>
+        );
+      };
 
     return (
         <div className="questions-container">
@@ -68,36 +100,115 @@ const Questions = () => {
                             ))}
                         </div>
                     }
-                    {question.optionType === 'dropdown' &&
-                        <select className="form-select form-select-sm" aria-label="Small select example" disabled>
+                    {question.optionType === 'dropdown' && (
+                        <select 
+                            className="form-select" 
+                            aria-label="Small select example" 
+                            value={question.options.find(option => option.isCorrect)?.text || ''}
+                        >
                             {question.options.map((option, index) => (
                                 <option 
                                     key={option._id} 
-                                    value={option.text} 
-                                    selected={option.isCorrect}>
+                                    value={option.text}>
                                     {option.text}
                                 </option>
                             ))}
                         </select>
-                    }
-                    {question.optionType === 'linear' &&
+                    )}
+                    {question.optionType === 'linear' && (
                         <div className="linear-scale">
-                            <div className="scale-label">Good</div>
-                            <input type="radio" name={`linearScale-${question._id}`} value="1" />
-                            <input type="radio" name={`linearScale-${question._id}`} value="2" />
-                            <input type="radio" name={`linearScale-${question._id}`} value="3" />
-                            <input type="radio" name={`linearScale-${question._id}`} value="4" />
-                            <input type="radio" name={`linearScale-${question._id}`} value="5" />
-                            <div className="scale-label">Bad</div>
+                            <div className="scale-label-left">{question.linearScale[0].label}</div>
+                            <div className="scale-options-horizontal">
+                                {
+                                    Array.from({ length: question.linearScale[1].scale - question.linearScale[0].scale + 1 }).map((_, index) => (
+                                        <label key={index} className="scale-option-horizontal">
+                                            {index + question.linearScale[0].scale}
+                                            <input type="radio" name={`linearScale-${question._id}`} value={index + question.linearScale[0].scale} disabled />
+                                        </label>
+                                    ))
+                                }
+                            </div>
+                            <div className="scale-label-right">{question.linearScale[1].label}</div>
                         </div>
-                    }
+                    )}
+                    {question.optionType === 'multipleChoiceGrid' && (
+                        <table className="multiple-choice-grid">
+                            <thead>
+                                <tr>
+                                    <th> </th>
+                                    {question.grid.columns.map((column, columnIndex) => (
+                                        <th key={columnIndex}>{column.text}</th>
+                                    ))}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {question.grid.rows.map((row, rowIndex) => (
+                                    <tr key={rowIndex}>
+                                        <td>{row.text}</td>
+                                        {question.grid.columns.map((column, columnIndex) => (
+                                            <td key={columnIndex}>
+                                                <input 
+                                                    type="radio" 
+                                                    name={`${row.text}-${column.text}`} 
+                                                    value={`${row.text}-${column.text}`}
+                                                    disabled={true}  
+                                                />
+                                            </td>
+                                        ))}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    )}
+                    {question.optionType === 'checkboxGrid' && (
+                        <table className="checkbox-grid">
+                            <thead>
+                                <tr>
+                                    <th> </th>
+                                    {question.grid.columns.map((column, columnIndex) => (
+                                        <th key={columnIndex}>{column.text}</th>
+                                    ))}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {question.grid.rows.map((row, rowIndex) => (
+                                    <tr key={rowIndex}>
+                                        <td>{row.text}</td>
+                                        {question.grid.columns.map((column, columnIndex) => (
+                                            <td key={columnIndex}>
+                                                <input 
+                                                    type="checkbox" 
+                                                    name={`${row.text}-${column.text}`} 
+                                                    value={`${row.text}-${column.text}`}
+                                                    disabled={true}  
+                                                />
+                                            </td>
+                                        ))}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    )}
                     {question.marks && <p className="question-marks">Marks: {question.marks}</p>}
                     {question.countries && question.countries.length > 0 && (
                     <p className="question-countries">Countries: {question.countries.join(', ')}</p>
                     )}
                     {question.explanation && <p className="question-explanation">Explanation: {question.explanation}</p>}
                     <p className="question-date">Date created: {formatDate(question.date)}</p>
-                    <EditQuestion />
+                    <div className="question-actions">
+                        <EditQuestion />
+                        <button 
+                            className="btn btn-dark" 
+                            onClick={() => handleDeleteClick(question._id)}>
+                            Delete
+                        </button>
+                        <ConfirmModal
+                            isOpen={isModalOpen}
+                            onClose={() => setIsModalOpen(false)}
+                            onConfirm={handleConfirmDelete}
+                            question={selectedQuestion}
+                        />
+                    </div>
                 </div>
             ))}
         </div>
