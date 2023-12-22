109f0a90103c110c1eaabddb5ad7b0646998cb44
Chen Shiang <chenshianglee@gmail.com>
2023-12-22 12:33:31 +0800

    create question

--- rethinkpack/src/view/createQuestion.js
+++ rethinkpack/src/view/createQuestion.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { Modal } from 'bootstrap';
@@ -98,6 +98,7 @@ class CreateQuestion extends Component {
 
   fetchQuestions = async () => {
     try {
+      console.log("test")
       const response = await fetch('http://localhost:5000/api/displayAllQuestions');
       const questions = await response.json();
       this.setState({ allQuestions: questions });
@@ -233,7 +234,7 @@ class CreateQuestion extends Component {
   };
 
   renderOptionsArea = () => {
-    const { selectedOption, options, gridOptions, requireResponse, isLeadingQuestion } = this.state;
+    const { selectedOption, options, gridOptions, requireResponse, isLeadingQuestion, allQuestions } = this.state;
     const clearSelections = (e) => {
       e.preventDefault();
       e.stopPropagation();
@@ -271,12 +272,23 @@ class CreateQuestion extends Component {
                   disabled={isLeadingQuestion}
                   onChange={() => this.toggleCorrectAnswer(index)}
                 />
-                <input
-                  type="text"
-                  className="form-control mx-2"
-                  onChange={(e) => this.handleOptionChange(index, e.target.value)}
-                  placeholder={`Option ${index + 1}`}
-                />
+                <div className="d-flex flex-grow-1 mx-2">
+                  <input
+                    type="text"
+                    className="form-control mx-2"
+                    onChange={(e) => this.handleOptionChange(index, e.target.value)}
+                    placeholder={`Option ${index + 1}`}
+                    style={{ flex: '1' }}
+                  />
+                  <select className="form-select mx-2" style={{ flex: '1' }}>
+                    <option value="">Select Next Question</option>
+                    {allQuestions.map((question) => (
+                      <option key={question._id} value={question._id}>
+                        {question.question}
+                      </option>
+                    ))}
+                  </select>
+                </div>
                 {options.length > 1 && (
                   <button
                     className="btn btn-outline-secondary"
