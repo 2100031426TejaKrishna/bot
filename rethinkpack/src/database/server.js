73081d02865cc98a10047d60b709d4c3a7b430b3
Pavan6136 <anonymouspavan007@gmail.com>
2024-07-11 21:35:48 +0530

    pavan-corsError

--- rethinkpack/src/database/server.js
+++ rethinkpack/src/database/server.js
@@ -25,6 +25,15 @@ const corsOptions = {
 };
 
 const app = express();
+app.options('*',cors());
+var allowCrossDomain = function(req,res,next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();  
+}
+app.use(allowCrossDomain);
+
 app.use(cors());
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
